29a78df20c77fc8d28ac3e721ca405e1d26fbf3e
Mean Dyna <konsomlanh123@outlook.com>
2021-03-13 20:27:02 +0700

    This code is perfected is Example of Tree recursion

--- src/com/codetoday/Main.java
+++ src/com/codetoday/Main.java
@@ -4,10 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-	// write your code here
+        // write your code here
+        call(3);
 
-       
 
 
     }
+    private  static void call(int value) {
+
+        if(value >0) {
+            System.out.println("Value is :: " + value );
+            call(value -1);
+            call(value -1);
+        }
+    }
+}
 
